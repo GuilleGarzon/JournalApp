45b1f5781ee6799f43a91f99cc906a08387df92a
Guillermo <guillegarzon.gg@gmail.com>
2024-01-03 20:26:27 -0500

    feat:Install MUI

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import { AppRouter } from './router/AppRouter';
-import { BrowserRouter } from 'react-router-dom';
 
 export const JournalApp = () => {
   return (
-    <>
-      <BrowserRouter>
-        <AppRouter />
-      </BrowserRouter>
+    <>      
+      <AppRouter />
     </>
   )
 }
