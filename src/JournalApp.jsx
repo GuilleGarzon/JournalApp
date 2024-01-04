bb3557bbaee6d413027cd3e814b876810a1a9649
Guillermo <guillegarzon.gg@gmail.com>
2024-01-03 19:53:15 -0500

    feat: Add routes

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { AppRouter } from './router/AppRouter';
+import { BrowserRouter } from 'react-router-dom';
 
 export const JournalApp = () => {
   return (
     <>
-      <h1>Hola Mundo</h1>
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
     </>
   )
 }
