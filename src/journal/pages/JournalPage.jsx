6f5eb32f5bf0bbbe5d58527fe4bee8219d2ef0df
Guillermo <guillegarzon.gg@gmail.com>
2024-01-10 18:00:03 -0500

    feat:Add button in JournalPage

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,3 +1,4 @@
+import { IconButton } from '@mui/material';
 import { JournalLayout } from '../layout/JournalLayout';
 import { NoteView, NothingSelectedView } from '../views';
 
@@ -7,8 +8,22 @@ export const JournalPage = () => {
       
       {/* <Typgraphy>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque qui quos tempora. Sequi doloremque aspernatur rem enim nulla. Numquam et earum perferendis eum omnis amet quidem sed deserunt soluta corrupti!</Typgraphy> */}
 
-      {/* <NothingSelectedView /> */}
-      <NoteView />
+      <NothingSelectedView />
+      {/* <NoteView /> */}
+
+      <IconButton
+        size='large'
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        {/* <Addoutlined sx={{ fontSize: 30 }}/> */}
+      </IconButton>
     </JournalLayout>
   )
 }
