8b3864d62612d077bc265d332fdfe36382911127
Guillermo <guillegarzon.gg@gmail.com>
2024-01-10 17:31:18 -0500

    feat: Add NoteView and component Image Gallery

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,5 +1,5 @@
 import { JournalLayout } from '../layout/JournalLayout';
-import { NothingSelectedView } from '../views';
+import { NoteView, NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
   return (
@@ -7,8 +7,8 @@ export const JournalPage = () => {
       
       {/* <Typgraphy>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque qui quos tempora. Sequi doloremque aspernatur rem enim nulla. Numquam et earum perferendis eum omnis amet quidem sed deserunt soluta corrupti!</Typgraphy> */}
 
-      <NothingSelectedView />
-      {/* Noteview */}
+      {/* <NothingSelectedView /> */}
+      <NoteView />
     </JournalLayout>
   )
 }
