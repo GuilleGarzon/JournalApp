b399a5254670b421fd25cfb6dfd5a0ea2ee11e80
Guillermo <guillegarzon.gg@gmail.com>
2024-01-10 13:23:43 -0500

    feat: Add NothingSeleted View in JournalPage

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,13 +1,13 @@
-import { Typography } from '@mui/material';
 import { JournalLayout } from '../layout/JournalLayout';
+import { NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
       
-      <Typography>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque qui quos tempora. Sequi doloremque aspernatur rem enim nulla. Numquam et earum perferendis eum omnis amet quidem sed deserunt soluta corrupti!</Typography>
+      {/* <Typgraphy>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque qui quos tempora. Sequi doloremque aspernatur rem enim nulla. Numquam et earum perferendis eum omnis amet quidem sed deserunt soluta corrupti!</Typgraphy> */}
 
-      {/* NothingSelected */}
+      <NothingSelectedView />
       {/* Noteview */}
     </JournalLayout>
   )
