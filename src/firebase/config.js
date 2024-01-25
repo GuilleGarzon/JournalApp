850f83e7a770ddeb68a4f67956446583485b9887
Guillermo <guillegarzon.gg@gmail.com>
2024-01-24 19:49:31 -0500

    feat: Google SignIn

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,8 +5,6 @@ import { getFirestore } from 'firebase/firestore/lite';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-console.log(process.env.FIREBASE_apiKey)
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_apiKey,
@@ -14,7 +12,8 @@ const firebaseConfig = {
   projectId: process.env.FIREBASE_projectId,
   storageBucket: process.env.FIREBASE_storageBucket,
   messagingSenderId: process.env.FIREBASE_messagingSenderId,
-  appId: process.envFIREBASE_appId,
+  appId: process.env.FIREBASE_appId,
+  
 };
 
 // Initialize Firebase
